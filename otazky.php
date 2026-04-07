d479faff7bb90e2286b2b366b6aefd5ee8fd2e68
Mirexon™ <savrda50@gmail.com>
2026-04-07 10:21:37 +0200

    Fix problems
    Change thankyou.html to php file.
    Fix error php loading $otazky

--- otazky.php
+++ otazky.php
@@ -1,17 +1,14 @@
 <?php
-//pole otázok
-$otazky = array{
-"Čo je PHP a na čo sa používa?"
-"Ako sa v PHP vypíše text na stránku?"
-"Ako sa v PHP vytvorí premenná?"
-};
+// pole otázok
+$otazky = array(
+    "Čo je PHP a na čo sa používa?",
+    "Ako sa v PHP vypíše text na stránku?",
+    "Ako sa v PHP vytvorí premenná?"
+);
 
-$odpovede = array{
-    "PHP je serverový programovací jazyk používaný na tvorbu dynamických webových stránok. Umožňuje spracovávať formuláre, pracovať s databázou a generovať HTML obsah."
-    "Text sa vypíše pomocou príkazu echo."
+$odpovede = array(
+    "PHP je serverový programovací jazyk používaný na tvorbu dynamických webových stránok. Umožňuje spracovávať formuláre, pracovať s databázou a generovať HTML obsah.",
+    "Text sa vypíše pomocou príkazu echo.",
     "Premenná v PHP začína znakom $."
-};
-
-
-
+);
 ?>
